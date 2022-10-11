194e169c214316da8ece7fa0ef93c8ec58b6bbcc
SivaKarthikPara <56120741+SivaKarthikPara@users.noreply.github.com>
2022-10-11 20:14:15 +0530

    Time: 7 ms (8.65%), Space: 61.1 MB (11.11%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,4 +1,5 @@
 class Solution {
+   /*// O(n) --> linear approach
     public int singleNonDuplicate(int[] nums) {
         if(nums.length == 1)
             return nums[0];
@@ -7,5 +8,14 @@ class Solution {
                 return nums[i];
         }
         return nums[nums.length-1];
+    }
+    */
+    // O(n) --> xor of all elements
+    public int singleNonDuplicate(int[] nums) {
+        int res = 0;
+        for(int i = 0; i < nums.length; i++){
+            res ^= nums[i];
+        }
+        return res;
     }
 }
