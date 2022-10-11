7b311bb92e88579af99211cac31b5fdb3a5cfba6
SivaKarthikPara <56120741+SivaKarthikPara@users.noreply.github.com>
2022-10-11 21:18:59 +0530

    Time: 0 ms (100.00%), Space: 48.1 MB (92.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -9,7 +9,7 @@ class Solution {
         }
         return nums[nums.length-1];
     }
-    */
+    
     // O(n) --> xor of all elements
     public int singleNonDuplicate(int[] nums) {
         int res = 0;
@@ -17,5 +17,20 @@ class Solution {
             res ^= nums[i];
         }
         return res;
+    }
+    */
+    //using binary search
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while(low < high){
+            int mid = low+(high-low)/2;
+            if(mid%2==1) mid--;
+            
+            if(nums[mid] == nums[mid+1])
+                low = mid+2;
+            else
+                high = mid;
+        }
+        return nums[low];
     }
 }
