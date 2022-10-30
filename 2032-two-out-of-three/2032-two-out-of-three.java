7e14e5e22ec5954df2deebaaca23a1b5c06c65fa
Siva Karthik Para <56120741+SivaKarthikPara@users.noreply.github.com>
2022-10-30 12:08:16 +0530

    Time: 17 ms (17.73%), Space: 42.7 MB (98.94%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        List<Integer> list1 = Arrays.stream(nums1).boxed().collect(Collectors.toList());
+        List<Integer> list2 = Arrays.stream(nums2).boxed().collect(Collectors.toList());
+        List<Integer> list3 = Arrays.stream(nums3).boxed().collect(Collectors.toList());
+        
+        Set<Integer> unionOfLists = new HashSet<>();
+        unionOfLists.addAll(list1);
+        unionOfLists.addAll(list2);
+        unionOfLists.addAll(list3);
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        for(Integer i : unionOfLists){
+            int c = 0;
+            if(list1.contains(i)) c++;
+            if(list2.contains(i)) c++;
+            if(list3.contains(i)) c++;
+            
+            if(c>=2) ans.add(i);
+        }
+        
+        return ans;
+    }
+}
