3097cdbcde6edcc3ef6daa5c01ffdd690c37d81d
Siva Karthik Para <56120741+SivaKarthikPara@users.noreply.github.com>
2022-12-04 17:28:36 +0530

    Time: 16 ms (52.12%), Space: 44.7 MB (44.91%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        int temp = x;
+        while(temp > 0){
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+        if(x == rev)
+            return true;
+        return false;
+    }
+}
